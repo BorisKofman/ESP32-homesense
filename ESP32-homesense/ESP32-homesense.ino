56f38ceba08624e922a0f4306ad6abe7021f75ee
BorisKofman <Borisk@salt.security>
2024-10-04 14:26:01 +0300

    small fix

--- ESP32-homesense/ESP32-homesense.ino
+++ ESP32-homesense/ESP32-homesense.ino
@@ -13,7 +13,6 @@ const int baudRate = 115200;  // Default baud rate for LD2412
 HardwareSerial radarSerial(1); 
 RadarType radar(radarSerial);  // Pass radarSerial to the LD2412 constructor
 const int dataBits = SERIAL_8N1;
-
 #elif defined(USE_LD2410)
 #include <ld2410.h>  
 typedef ld2410 RadarType;
@@ -21,23 +20,17 @@ const int baudRate = 256000;
 HardwareSerial radarSerial(1); 
 RadarType radar;  // Radar for LD2410 (no constructor call)
 const int dataBits = SERIAL_8N1;
-
 #elif defined(USE_LD2450)
 #include "LD2450.h" 
 typedef LD2450 RadarType;
 const int baudRate = 256000;
 HardwareSerial radarSerial(1); 
 RadarType radar; 
-
+const int dataBits = SERIAL_8N1;
 #endif
-
-
 
 #include "RadarAccessory.h" 
 #include "VirtualSwitch.h"
-
-
-const int dataBits = SERIAL_8N1;
 
 void setup() {
   Serial.begin(115200);
